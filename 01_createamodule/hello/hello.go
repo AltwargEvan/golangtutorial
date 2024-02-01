a7df0c7a0e52e8bb2c5ea4c243dbe05987b509fc
Evan <evanaltwarg2021@gmail.com>
2024-01-31 20:47:16 -0500

    feat: :sparkles: complete Return and handle an error

--- 01_createamodule/hello/hello.go
+++ 01_createamodule/hello/hello.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
     // Get a greeting message and print it.
-    message := greetings.Hello("Evan")
+    message, err:= greetings.Hello("")
+    if(err != nil) {
+        log.Fatal(err)
+    }
     fmt.Println(message)
 }
