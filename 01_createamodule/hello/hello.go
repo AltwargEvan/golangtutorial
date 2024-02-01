e7389bf4b52eb1c8351bf41dce74d7e6f9f329f1
Evan <evanaltwarg2021@gmail.com>
2024-01-31 20:49:30 -0500

    feat: :sparkles: complete return a random greeting

--- 01_createamodule/hello/hello.go
+++ 01_createamodule/hello/hello.go
@@ -8,10 +8,21 @@ import (
 )
 
 func main() {
-    // Get a greeting message and print it.
-    message, err:= greetings.Hello("")
-    if(err != nil) {
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+    // Request a greeting message.
+    message, err := greetings.Hello("Evan")
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
         log.Fatal(err)
     }
+
+    // If no error was returned, print the returned message
+    // to the console.
     fmt.Println(message)
 }
